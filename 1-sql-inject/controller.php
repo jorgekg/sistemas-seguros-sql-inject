2d9f84af6c6ba02e7bce79d682e1800b3b75b579
Jorge Guilherme <jorgekg3@gmail.com>
2019-03-17 14:32:15 -0300

    commit final

--- 1-sql-inject/controller.php
+++ 1-sql-inject/controller.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=trab', "root", "faccao12");
+  $conn = new PDO('mysql:host=127.0.0.1;dbname=trab', "root", "root");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
@@ -13,4 +13,6 @@ $data = $conn->query("SELECT * FROM usuario WHERE email = '". $email ."' and sen
 
 if (!empty($data->fetchAll())) {
 	header("location: ok.php");
+} else {
+  header("location: login.html");
 }
--- 3-sql-inject-bind/controller.php
+++ 3-sql-inject-bind/controller.php
@@ -1,18 +1,20 @@
 <?php
 try {
-  $conn = new PDO('mysql:host=127.0.0.1;dbname=trab', "root", "faccao12");
+  $conn = new PDO('mysql:host=127.0.0.1;dbname=trab', "root", "root");
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $email = $_GET["email"];
+  $senha = $_GET["senha"];
+
+  $data = $conn->prepare("SELECT * FROM usuario WHERE email = :email and senha = :senha");
+  $data->bindParam(':email', $email);
+  $data->bindParam(':senha', $senha);
+  $data->execute();
+  if (!empty($data->fetchAll())) {
+    header("location: ok.php");
+  } else {
+    header("location: login.html");
+  }
+  exit;
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
-}
-
-$email = $_GET["email"];
-$senha = $_GET["senha"];
-
-$data = $conn->prepare("SELECT * FROM usuario WHERE email = ? and senha = ?");
-$data->bindValue(1, $email);
-$data->bindValue(2, $senha);
-
-if (!empty($data->fetchAll())) {
-	header("location: ok.php");
 }
